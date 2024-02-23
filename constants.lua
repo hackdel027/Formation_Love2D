b15411018c7bc2fd9e78a443f8d3abead4acee52
dean3r1s <jardelm027@gmail.com>
2024-02-23 08:31:59 +0100

    gestion des animations avec anim8 et deplacement du personnage.

---
+++ constants.lua
@@ -0,0 +1,3 @@
+WIN_HEIGHT = love.graphics.getHeight()
+WIN_WIDTH = love.graphics.getWidth()
+WIN_TITLE = "Legend Of Jardel"
---
+++ main.lua
@@ -0,0 +1,58 @@
+require('constants')
+local Player
+
+function love.load()
+    -- love.graphics.setColor(255, 255, 255)
+    
+    local anim8 = require 'lib/anim8'
+    Player = {}
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    love.graphics.setBackgroundColor(0, 0.2, 0.3)
+    love.window.setTitle(WIN_TITLE)
+    Player.x = WIN_WIDTH / 2
+    Player.y = WIN_HEIGHT / 2
+    Player.speed = 100
+    -- Player.sprite = love.graphics.newImage('sprites/parrot.png')
+    Player.sprite = love.graphics.newImage('sprites/player-sheet.png')
+    Player.grid = anim8.newGrid(12, 18, Player.sprite:getWidth(), Player.sprite:getHeight())
+    Player.animation = {}
+    Player.animation.down = anim8.newAnimation(Player.grid('1-4', 1), 0.2)
+    Player.animation.left = anim8.newAnimation(Player.grid('1-4', 2), 0.2)
+    Player.animation.right = anim8.newAnimation(Player.grid('1-4', 3), 0.2)
+    Player.animation.up = anim8.newAnimation(Player.grid('1-4', 4), 0.2)
+    Player.anim = Player.animation.right
+end
+
+function love.update(dt)
+    local isMoving = false
+    if love.keyboard.isDown('left', 'a') and Player.x > 0 then
+        Player.x = Player.x - (Player.speed * dt)
+        Player.anim = Player.animation.left
+        isMoving = true
+    end 
+    if love.keyboard.isDown('right', 'd') and Player.x + Player.sprite:getWidth()< WIN_WIDTH then
+        Player.x = Player.x + (Player.speed * dt)
+        Player.anim = Player.animation.right
+        isMoving = true
+    end
+    if love.keyboard.isDown('up', 'w') and Player.y > 0 then
+        Player.anim = Player.animation.up
+        Player.y = Player.y - (Player.speed * dt)
+        isMoving = true
+    end
+    if love.keyboard.isDown('down', 's') and Player.y + Player.sprite:getHeight() < WIN_HEIGHT then
+        Player.anim = Player.animation.down
+        Player.y = Player.y + (Player.speed * dt)
+        isMoving = true
+    end
+    if isMoving == false then
+        Player.anim:gotoFrame(2)
+    end
+    Player.anim:update(dt)
+end
+
+function love.draw()
+    -- love.graphics.draw(Player.sprite, Player.x, Player.y)
+    Player.anim:draw(Player.sprite, Player.x, Player.y, nil, 5)
+end
+
